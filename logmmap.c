e8703b86d5e33a31362ff72ef52107c7ff297226
giambagiamba <giambagiamba60@gmail.com>
2021-01-05 23:24:05 +0100

    Added i and Num in struct. pbar_draw re-fashioned.

--- logmmap.c
+++ logmmap.c
@@ -16,6 +16,9 @@
 typedef struct{
 	char* bar;
 	unsigned int len;
+	//unsigned int perc;
+	unsigned int i;
+	unsigned int Num;
 	unsigned int perc;
 	unsigned int nblks;
 	unsigned int err;
@@ -28,13 +31,14 @@ typedef struct{
 //gia' preparati prima con mmap.
 //Prevede barra lunga len e percentuale (xx%) e accapo a terminare.
 void pbar_init(pbar* progbar){
-	unsigned int i, len, perc, nblks;
+	unsigned int i, Num, len, perc, nblks;
+	unsigned int u, d, c;
 	char* mem;
 	int file;
 	
 	progbar->err=0;
 	len=progbar->len;
-	perc=progbar->perc;
+	//perc=progbar->perc;
 
 	file=open(progbar->filename, O_RDWR|O_CREAT, S_IRWXU|S_IRWXG|S_IRWXO);
 	if(file<0){
@@ -61,7 +65,9 @@ void pbar_init(pbar* progbar){
 	
 	progbar->bar=mem;
 
-	if(perc<0 || perc>100){
+	i=progbar->i;
+	Num=progbar->Num;
+	if(i<0 || i>Num-1){
 		progbar->err|=WARN_PERC;
 		perc=0;
 		//return;
@@ -72,7 +78,7 @@ void pbar_init(pbar* progbar){
 		//return;
 	}
 	
-	nblks=perc*len/100;
+	nblks=(long unsigned int)i*len/(Num-1);
 	progbar->nblks=nblks;
 	mem[0]='[';
 	for(i=1;i<nblks+1;i++){
@@ -82,9 +88,19 @@ void pbar_init(pbar* progbar){
 		mem[i]=' ';
 	}
 	mem[len+1]=']';
-	mem[len+2]=(perc/100)+'0';
-	mem[len+3]=((perc/10)%10)+'0';
-	mem[len+4]=(perc%10)+'0';
+	
+	perc=(long unsigned int)i*100/(Num-1);
+	progbar->perc=perc;
+	u=perc%10;
+	d=(perc/10)%10;
+	c=perc/100;
+	mem[len+2]=c*(1+'0'-' ')+' ';
+	mem[len+3]=(c+d==0) ? ' ' : d+'0';//Migliorare
+	mem[len+4]=u+'0';
+	
+	//mem[len+2]=c+'0';
+	//mem[len+3]=((perc/10)%10)+'0';
+	//mem[len+4]=u+'0';
 	mem[len+5]='%';
 	mem[len+6]=0xa;
 	
@@ -95,32 +111,95 @@ void pbar_init(pbar* progbar){
 
 void pbar_draw(pbar* progbar){
 	char* mem = progbar->bar;
-	unsigned int nblks, len=progbar->len, perc=progbar->perc;
+	unsigned int nblks, perc, len=progbar->len, i=progbar->i, Num=progbar->Num;
+	unsigned int u, d, c;
+
+	perc=(long unsigned int)i*100/(Num-1);//Intermediate long to avoid overflow
 
 	if(mem == 0){
 		progbar->err|=ERR_MEM;
 		return;
 	}
-	if(perc<0 || perc>100){
+	if(i>(Num-1)){
 	     	progbar->err|=WARN_PERC;
 		perc=0;
 	}
 	
-	nblks=perc*len/100; //New number of blocks
-	//progbar->bar=mem;
-	if(nblks==(progbar->nblks)+1){ //If new nblks == old one+1...
-		progbar->nblks=nblks; //Update nblks
-		mem[nblks]='|'; //Draw another block
-	}
+	//printf("Perc= %u\t%u\n", i, perc);
+	if(perc==progbar->perc) return;//Same percentage, nothing to do
 	else{
-		//TODO
+		//printf("\tqui\n");
+		nblks=(long unsigned int)i*len/(Num-1); //Calculate new number of blocks
+		progbar->perc=perc;//Update stored perc
+		//if(nblks==progbar->nblks){//Same nblks, just update percentage
+		//	u=perc%10;
+                //        d=(perc/10)%10;
+                //        c=perc/100;
+                //        mem[len+2]=c*(1+'0'-' ')+' ';
+                //        mem[len+3]=(c+d==0) ? ' ' : d+'0';//Migliorare
+                //        mem[len+4]=u+'0';
+                //        return;
+                //}
+		if(nblks==(progbar->nblks)+1){ //Add one block
+			//printf("\t\tnewblock\n");
+	       	        mem[nblks]='|'; //Draw another block
+	        }
+		else if(nblks!=progbar->nblks){//Not the same, redraw whole bar
+			for(i=0;i<nblks;i++){
+                	        mem[i]='|';
+	                }
+        	        for(;i<len+1;i++){
+        	                mem[i]=' ';
+	                }
+		}
+		progbar->nblks=nblks; //Update stored nblks
+		u=perc%10;
+		d=(perc/10)%10;
+		c=perc/100;
+		mem[len+2]=c*(1+'0'-' ')+' ';
+		mem[len+3]=(c+d==0) ? ' ' : d+'0';//Migliorare
+		mem[len+4]=u+'0';
+
 	}
-	//mem[len+2]=(perc/100)*(1+'0')-' '*(perc/100-1);
-	mem[len+2]=(perc/100)*(1+'0'-' ')+' ';
-	mem[len+3]=((perc/10)%10)+'0';
-	mem[len+4]=(perc%10)+'0';
 
 	return;
+
+
+//	if(nblks==progbar->nblks){//Ottimizzazione
+//		if(perc==progbar->perc)	return;
+//		else{
+//			u=perc%10;
+//		        d=(perc/10)%10;
+//			c=perc/100;
+//			mem[len+2]=c*(1+'0'-' ')+' ';
+//			mem[len+3]=(c+d==0) ? ' ' : d+'0';//Migliorare
+//			mem[len+4]=u+'0';
+//			progbar->perc=perc;
+//
+//			return;
+//		}
+//	}
+//	if(nblks==(progbar->nblks)+1){ //If new nblks == old one+1...
+//		progbar->nblks=nblks; //Update nblks
+//		mem[nblks]='|'; //Draw another block
+//	}
+//	else if(nblks!=progbar->nblks){//Else redraw whole bar
+//		for(i=0;i<nblks;i++){
+//			mem[i]='|';
+//		}
+//		for(;i<len+1;i++){
+//			mem[i]=' ';
+//		}
+//	}
+//	progbar->perc=perc;
+//	u=perc%10;
+//	d=(perc/10)%10;
+//	c=perc/100;
+//	mem[len+2]=c*(1+'0'-' ')+' ';
+//	mem[len+3]=(c+d==0) ? ' ' : d+'0';//Migliorare
+//	mem[len+4]=u+'0';
+//
+//	return;
 }
 
 void pbar_close(pbar* progbar){
