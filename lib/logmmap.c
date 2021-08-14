81d25de5dfbce6fdb322fc9260b711c4f9a8feed
giamba <giambagiamba60@gmail.com>
2021-08-14 15:09:02 +0200

    Test ssh push

--- lib/logmmap.c
+++ lib/logmmap.c
@@ -15,7 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
- 
+
+//Modification to test ssh-based push
+
 #ifndef LOGMMAP_C
 #define LOGMMAP_C
 
